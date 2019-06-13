3703ecb2b68dc6da5200255536e6c1f940dad34f
603822 <haroutboujakjian@FVFYJ0C3HV2G.fios-router.home>
2019-06-13 12:22:49 -0400

    added exec-path-from-shell for mac osx and jupyter interpreter in comments

--- init.el
+++ init.el
@@ -20,13 +20,19 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ess virtualenvwrapper elpy jedi flycheck zenburn-theme which-key use-package try ox-reveal org-bullets htmlize auto-complete))))
+    (exec-path-from-shell ess virtualenvwrapper elpy jedi flycheck zenburn-theme which-key use-package try ox-reveal org-bullets htmlize auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(use-package exec-path-from-shell
+  :ensure t)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
 
 (visual-line-mode t) ;soft wrap for lines
 
@@ -126,6 +132,10 @@
 (require 'python)
 (setq python-shell-interpreter "ipython")
 (setq python-shell-interpreter-args "--simple-prompt -i --pylab")
+
+;; (setq python-shell-interpreter "jupyter"
+;;       python-shell-interpreter-args "console --simple-prompt")
+;; (setq python-shell-completion-native-enable nil)
 
 (use-package elpy
   :ensure t
