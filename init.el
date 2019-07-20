eaa15d5214956bc6a15fcf8b1354a8571eac4050
squarerout <haroutb719@gmail.com>
2019-07-20 12:04:48 -0400

    created web-mode-hook to decrease indent from 4 to 2

--- init.el
+++ init.el
@@ -185,17 +185,26 @@
   (setq web-mode-ac-sources-alist
 				'(("css" . (ac-source-css-property))
 					("html" . (ac-source-words-in-buffer ac-source-abbrev))
-					("js" . (ac-js2))))
+					("js" . (ac-js2-mode))))
   )
+
+(defun my-web-mode-hook()
+	"Hooks for Web mode. Adjust indents"
+	(setq web-mode-markup-indent-offset 2)
+	(setq web-mode-css-indent-offset 2)
+	(setq web-mode-code-indent-offset 2)
+	)
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
 
 ;; (use-package js2-mode
 ;;   :ensure t
 ;;   :ensure ac-js2
 ;;   :init
 ;;   (progn
-;; 	(add-hook 'js-mode-hook 'js2-minor-mode)
-;; 	(add-hook 'js2-mode-hook 'ac-js2-mode)
-;; 	))
+;; 		(add-hook 'js-mode-hook 'js2-minor-mode)
+;; 		(add-hook 'js2-mode-hook 'ac-js2-mode)
+;; 		))
 
 (use-package json-mode
   :ensure t) ;; additional syntax highlighting on top of js-mode
