ddbc5c48398c6929cb9872a734848871ce01f532
squarerout <haroutb719@gmail.com>
2019-07-31 20:57:02 -0400

    testing out open shell in current buffer

--- init.el
+++ init.el
@@ -56,6 +56,7 @@
 (global-hl-line-mode t) ;; highlights current line of cursor
 ;;(add-hook 'python-mode-hook 'linum-mode) ;; displaying line numbers
 
+(push (cons "\\*shell\\*" display-buffer--same-window-action) display-buffer-alist)
 
 (setq frame-resize-pixelwise t)
 (set-frame-position (selected-frame) 4 5)
